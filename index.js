cc434e3a8529182900e09e67fb54fa087a7baebb
aimonimran <aimon.imran@gmail.com>
2022-07-15 21:57:59 +0500

    Built rentals API

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
 const home = require('./routes/home');
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
 app.use('/', home);
 
 const port = process.env.PORT || 3000;
---
+++ models/rental.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const Rental = mongoose.model('Rental', new mongoose.Schema({
+    customer: {
+        type: new mongoose.Schema({
+            isGold: {
+                type: Boolean,
+                default: false
+            },
+            name: {
+                type: String,
+                required: true,
+                minlength: 3,
+                maxlength: 30
+            },
+            phone: {
+                type: String,
+                required: true,
+                minlength: 3,
+                maxlength: 30
+            }
+        }),
+        required: true
+    },
+    movie: {
+        type: new mongoose.Schema({
+            title: {
+                type: String,
+                required: true,
+                trim: true,
+                minlength: 5,
+                maxlength: 255
+            },
+            dailyRentalRate: {
+                type: Number,
+                required: true,
+                minlength: 0,
+                maxlength: 255
+            }
+        }),
+        required: true
+    },
+    dateOut: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    dateReturned: {
+        type: Date
+    },
+    rentalFee: {
+        type: Number,
+        min: 0
+    }
+}));
+
+const validateRental = rental => {
+    const schema = {
+        customerId: Joi.string().required(),
+        movieId: Joi.string().required()
+    };
+
+    return Joi.validate(rental, schema);
+};
+
+exports.Rental = Rental;
+exports.validate = validateRental;
